d2db2dc6d210bfa1cbd748c9a14241b87955b15d
Mauro <gagginaspinnata@gmail.com>
2019-09-16 11:15:48 +0200

    tolte parentesi

--- vpsmanagersetup.sh
+++ vpsmanagersetup.sh
@@ -128,7 +128,9 @@ then
 	echo "Port 443" >> /etc/ssh/sshd_config
 	grep -v "^PasswordAuthentication yes" /etc/ssh/sshd_config > /tmp/passlogin && mv /tmp/passlogin /etc/ssh/sshd_config
 	echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
-	INSTALL_BIN()
+
+	INSTALL_BIN
+
 	if [ ! -f "/etc/init.d/squid3" ]
 	then
 		service squid3 reload > /dev/null
@@ -154,7 +156,9 @@ then
 	echo "Port 443" >> /etc/ssh/sshd_config
 	grep -v "^PasswordAuthentication yes" /etc/ssh/sshd_config > /tmp/passlogin && mv /tmp/passlogin /etc/ssh/sshd_config
 	echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
-	INSTALL_BIN()
+
+	INSTALL_BIN
+
 	if [ ! -f "/etc/init.d/squid" ]
 	then
 		service squid reload > /dev/null
